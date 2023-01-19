c8ea83407084c44b05fc3e8337d0307e459f7cfe
thiago733 <thiago733@gmail.com>
2023-01-19 11:48:23 -0400

    inserindo comentarios

--- postInstall.sh
+++ postInstall.sh
@@ -1,27 +1,31 @@
+#Update
 sudo apt update && apt
-sudo apt install ssh 
+sudo apt install ssh curl vim
+#Instala a fonte Firacode
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.0/FiraCode.zip   
 unzip FiraCode.zip   
 mkdir -p ~/.local/share/fonts  
 mv ./*.ttf ~/.local/share/fonts/  
 fc-cache -f -v  
-sudo apt install curl 
+#Instala o terminal Starship
 curl -sS https://starship.rs/install.sh | 
 vi ~/.bashrc   
+#Instala o docker
 curl -fsSL https://get.docker.com | 
 sudo sh -eux <<EOF 
 # Install newuidmap & binaries
 apt-get install -y uidmap 
 EOF    
 docker run hello-world  
+#Instala o Kind
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64 
 chmod +x ./kind  
 sudo mv ./kind /usr/local/bin/kind 
-sudo apt install vim 
 sudo vim /etc/default/grub  
 sudo update-grub   
 micro /etc/systemd/system/user@.service.d/delegate.conf   
 sudo systemctl daemon-reload  
+#Instala o Kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s 
 chmod +x ./kubectl  
 sudo mv ./kubectl /usr/local/bin/kubectl 
